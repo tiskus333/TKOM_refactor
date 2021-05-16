84a431e624eeb2fdc06c88d7d092f4e0af6df97e
Mateusz Chrusciel <tiskus.3009@gmail.com>
2021-05-16 12:21:03 +0200

    empty tests

---
+++ tests/parserTest/test_parser.py
@@ -0,0 +1,62 @@
+import unittest
+from Parser.parser import Parser
+from errors import ParserError
+
+
+class TestParser(unittest.TestCase):
+    def test_Assignments(self):
+        pass
+
+    def test_ClassDefinition(self):
+        pass
+
+    def test_FunctionDefinition(self):
+        pass
+
+    def test_VariableDefinition(self):
+        pass
+
+    def test_ParameterDefinition(self):
+        pass
+
+    def test_IfStatement(self):
+        pass
+
+    def test_WhileStatement(self):
+        pass
+
+    def test_ReturnStatement(self):
+        pass
+
+    def test_StatementBlock(self):
+        pass
+
+    def test_BaseExpression(self):
+        pass
+
+    def test_MathExpression(self):
+        pass
+
+    def test_ParenthesesExpression(self):
+        pass
+
+    def test_FunctionCall(self):
+        pass
+
+    def test_BaseCondition(self):
+        pass
+
+    def test_RelationCondition(self):
+        pass
+
+    def test_ParenthesesCondition(self):
+        pass
+
+    def test_VariableAccess(self):
+        pass
+
+    def test_Negation(self):
+        pass
+
+    def test_LogicNegation(self):
+        pass
